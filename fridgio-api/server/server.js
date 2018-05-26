8bd2e2eb46726de873bab110f6c0fe2422e80fc6
Lucas do Lago <dolagolucas@gmail.com>
2018-05-26 19:58:44 -0300

    login

--- fridgio-api/server/server.js
+++ fridgio-api/server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var jwt = require('jsonwebtoken');
 var path = require('path');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -15,6 +16,39 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/login',(req,res)=>{
+	Usuario.find({username: req.body.username, senha: req.body.senha}).then((usuario)=>{
+
+		if (usuario.length !== 0){
+			jwt.sign({user: usuario}, 'secretkey',(err,token)=>{
+				res.send({username: req.body.username, sucess: true, token: token, message: 'Login efeituado com sucesso'});
+			});
+		}else{
+			res.send({sucess: false, message:"Usuário e/ou senha incorretos!"});
+		}
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+function verifyToken(req,res,next){
+	//Get auth header value
+	const bearerHeader = req.headers["authorization"];
+	//CHeck if bearer is undefined
+	if(typeof bearerHeader !== 'undefined'){
+		//Split at the space
+		const bearer = bearerHeader.split(' ');
+		//Get token from array
+		const bearerToken = bearer[1];
+		//Set the token
+		req.token = bearerToken;
+		next();
+	}else{
+		res.sendStatus(403);
+	}
+}
+
 
 app.get('/usuarios',(req,res)=>{
 	Usuario.find().then((usuarios)=>{
@@ -71,13 +105,19 @@ app.get('/receitas/add',(req, res)=>{
 		res.sendFile(path.join(__dirname, '../views', 'add.html'));
 });
 
-app.get('/receitas',(req,res)=>{
-	Receita.find({status: 1}).then((receitas)=>{
-		res.send(receitas);
-		//res.render(path.join(__dirname, '../views', 'lista-receitas.ejs'), {receitas: receitas});
-	},(e)=>{
-		res.status(400).send(e);
-	});
+app.get('/receitas',verifyToken, (req,res)=>{
+	jwt.verify(req.token,'secretkey',(err, authData)=>{
+		if(err){
+			res.sendStatus(403);
+		}else{
+			Receita.find({status: 1}).then((receitas)=>{
+				res.send(receitas);
+				//res.render(path.join(__dirname, '../views', 'lista-receitas.ejs'), {receitas: receitas});
+			},(e)=>{
+				res.status(400).send(e);
+			});
+		}
+	})
 });
 
 
