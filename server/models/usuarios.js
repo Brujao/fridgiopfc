b542ceda338cc46cd8851d667dad8e8eeaf192aa
Lucas do Lago <dolagolucas@gmail.com>
2018-07-21 14:34:34 -0300

    umbler host

---
+++ server/models/usuarios.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var usuarioSchema = mongoose.Schema({
+	username:{
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true,
+		lowercase: true,
+		index:{
+			unique: true,
+			dropUps: true
+		}
+	},
+	email:{
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 1
+	},
+	senha:{
+		type: String,
+		required: true,
+		minlength: 1
+	}
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
