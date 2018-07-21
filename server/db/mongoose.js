0342f99ff12a8952758ffd72e5dd7e30dcf749d3
Lucas do Lago <dolagolucas@gmail.com>
2018-07-21 15:06:35 -0300

    mongodb fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Foods');
+mongoose.connect('mongodb://cursedstudio:curs3dSTD@naboo.mongodb.umbler.com:38845/foods');
 
 module.exports = {mongoose};
