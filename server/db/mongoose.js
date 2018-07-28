5bf43adade4b33a24a4b6f1f751a40cbfcd1968f
dolago2k <dolago2k@gmail.com>
2018-07-28 00:12:41 -0300

    db update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://cursedstudio:curs3dSTD@naboo.mongodb.umbler.com:40241/foods');
+mongoose.connect('mongodb://cursedstudio:curs3dSTD@naboo.mongodb.umbler.com:42115/fridgio');
 
 module.exports = {mongoose};
