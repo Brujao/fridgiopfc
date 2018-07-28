e601a2f2e5b905c3b4a08551f430bee9c896666f
dolago2k <dolago2k@gmail.com>
2018-07-27 22:35:49 -0300

    api

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://cursedstudio:curs3dSTD@naboo.mongodb.umbler.com:40496/foods');
+mongoose.connect('mongodb://cursedstudio:curs3dSTD@naboo.mongodb.umbler.com:40241/foods');
 
 module.exports = {mongoose};
