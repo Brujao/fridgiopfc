b90fb81d864d9e286d4dd14a4d51fdb34f572d40
dolago2k <dolagolucas@gmail.com>
2018-07-30 11:47:35 -0300

    css path again

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 
-app.use(express.static(path.join(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, '../public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
