2b0c9d3a338e928e7828733a236b108bf8260919
dolago2k <dolago2k@gmail.com>
2018-08-03 22:34:07 -0300

    query fix v10

--- server/server.js
+++ server/server.js
@@ -130,7 +130,8 @@ app.post('/api/query', (req,res)=>{
 	// 	if(err){
 	// 		res.sendStatus(403);
 	// 	}else{
-			Receita.find({status: 1, ingredientes:{ $not: { $elemMatch: { $nin: [req.body.query] } } }}).then((receitas)=>{
+	var query = req.body.query;
+			Receita.find({status: 1, ingredientes:{ $not: { $elemMatch: { $nin: [query] } } }}).then((receitas)=>{
 				res.send(receitas);
 				//res.render(path.join(__dirname, '../views', 'lista-receitas.ejs'), {receitas: receitas});
 			},(e)=>{
