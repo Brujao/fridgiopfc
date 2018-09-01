412738e186057885c5da0f85a4374b3fddfc28f8
dolago2k <dolago2k@gmail.com>
2018-09-01 17:02:15 -0300

    push

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://cursedstudio:curs3dSTD@naboo.mongodb.umbler.com:49236/fridgio');
+mongoose.connect('mongodb://cursedstudio:curs3dSTD@tatooine.mongodb.umbler.com:49236/fridgio');
 
 module.exports = {mongoose};
