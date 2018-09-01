3cdd16632e4c3c228630d9cc619bd033316c9517
dolago2k <dolago2k@gmail.com>
2018-09-01 19:01:08 -0300

    query

--- server/server.js
+++ server/server.js
@@ -130,9 +130,8 @@ app.post('/api/query', (req,res)=>{
 	// 	if(err){
 	// 		res.sendStatus(403);
 	// 	}else{
-			var query = req.body.query.toString().split('"').join("/");
-			var exp = new RegExp(query);
-			Receita.find({status: 1, ingredientes:{ $not: { $elemMatch: { $nin: exp } } }}).then((receitas)=>{
+			var query = req.body.query;
+			Receita.find({status: 1, ingredientes:{ $not: { $elemMatch: { $nin: query } } }}).then((receitas)=>{
 				res.send(receitas);
 				//res.render(path.join(__dirname, '../views', 'lista-receitas.ejs'), {receitas: receitas});
 			},(e)=>{
