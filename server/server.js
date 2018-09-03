f0ffab1761e38cc400e81762a0b081be0695768f
dolago2k <dolago2k@gmail.com>
2018-09-03 15:41:55 -0300

    login admin

--- server/server.js
+++ server/server.js
@@ -44,7 +44,10 @@ app.post('/api/login',(req,res)=>{
 
 app.post('/api/loginAdmin',(req,res)=>{
     Usuario.find({username: req.body.username, senha: req.body.senha, status:1}).then((usuario) =>{
-			res.redirect('/api');
+
+			if (usuario.status == 1){
+				res.redirect('/api');
+			}
 		});
 });
 
