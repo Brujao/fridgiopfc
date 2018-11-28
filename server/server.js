94e661ea64879815d057411f0e3b18c49656bfd0
dolago2k <dolagolucas@gmail.com>
2018-11-28 10:21:30 -0200

    'changes'

--- server/server.js
+++ server/server.js
@@ -226,8 +226,7 @@ app.post('/api/aprovacao', upload.single('file'), (req, res) => {
 Receita.findByIdAndUpdate(req.body.id,{
 	$set: {
 		titulo:req.body.titulo,
-    foto.data: fs.readFileSync(req.files.path),
-    foto.contentType: 'image/png',
+    foto: req.file.path,
 		ingredientes:req.body.ingredientes.split(','), //
 		modoPreparo:req.body.modoPreparo,
 		status:req.body.status
