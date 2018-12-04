dd013d270074d4f045e80913f911f0a6363ed4a4
dolago2k <dolagolucas@gmail.com>
2018-12-04 00:13:39 -0200

    'changes'

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ var fs = require('fs');
 var multer = require('multer');
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, '/uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now()+'-'+file.ogirinalname)
+    cb(null, Date.now()+'-'+file.originalname)
   }
 });
 
